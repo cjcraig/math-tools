3a79beec567f1d2b90922ee5e0c56f1c6a802860
cjcraig <clytncraig@gmail.com>
2021-01-06 12:06:28 -0800

    Created method to return prime factorization

--- mathobj.py
+++ mathobj.py
@@ -3,31 +3,61 @@ This module will contain classes for mathematical objects that need their own me
 For example, a number in simple-radical form should have ways to display
 """
 from math import floor
+from fractions import Fraction
 
 
 class RadicalNum:
     """
     Class to handle numbers in simple radical form.
     """
-    # TODO unfinished method
 
-    def radform(self, input_number):
+    def __init__(self, input_number):
+        # We should make sure we are starting with a rational number
+        if isinstance(input_number, int) or isinstance(input_number, Fraction):
+            self.number = input_number
+        else:
+            raise TypeError(
+                "Simple radical form only makes sense applied to rationals!")
+
+    # TODO finish this method
+
+    def radform(self):
         """
         This function takes in a number, and returns it in simple radical form.
         """
 
         # Output format: [integer,radical]
         outpair = [1, 1]
-        root = input_number**0.5
+        root = self.number**0.5
 
         # If we have a perfect square, might as well just return the root
-        if root*root == input_number:
-                outpair[0] = root
-                return outpair
+        if root*root == self.number:
+            outpair[0] = root
+            return outpair
 
-            # First, we need to extract any perfect square factor
-            square_factors = []
+        # First, we need to extract any perfect square factor
+        square_factors = []
 
-            for i in range(4, floor(input_number/2), 1):
-                if input_number % i == 0 and i*i == i**0.5:
-                    square_factors.append(i)
+        for i in range(4, floor(self.number/2), 1):
+            if self.number % i == 0 and i*i == i**0.5:
+                square_factors.append(i)
+
+
+def get_factors(number):
+    """
+    Method to get a list of factors, repeated values allowed.
+    """
+    factor_list = []
+    while number % 2 == 0:
+        factor_list.append(2)
+        number = number/2
+
+    for factor in range(3, number/2, 2):
+        while number % factor == 0:
+            factor_list.append(factor)
+            number = number/factor
+        if number == 1:
+            break
+    if number != 1:
+        factor_list.append(number)
+    return factor_list
